8fe0c33eadcc3e96e1496862165327724f1fc969
Math54500 <Maths54500z@gmail.com>
2026-04-01 13:41:27 +0200

    ajout des usecases

---
+++ services/service-galeries/app/src/application_core/application/usecases/UnpublishGalerieUseCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\src\application_core\application\usecases;
+
+use app\src\application_core\application\ports\repositories\GalerieRepositoryInterface;
+
+class UnpublishGalerieUseCase
+{
+    private GalerieRepositoryInterface $repo;
+
+    public function __construct(GalerieRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function execute(string $galleryId, string $userId): void
+    {
+        $this->repo->unpublishGallery($galleryId, $userId);
+    }
+}
