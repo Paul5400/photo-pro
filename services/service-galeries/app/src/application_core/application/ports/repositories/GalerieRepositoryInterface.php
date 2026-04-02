1d88028f5a24cb9fe75848ef889dbb36db537e23
faozianikiema <nikiemaf37@gmail.com>
2026-04-02 09:55:06 +0200

    fix(les modifications apres le merge)

---
+++ services/service-galeries/app/src/application_core/application/ports/repositories/GalerieRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace photopro\galeries\core\application\ports\repositories;
+use photopro\galeries\core\application\dto\GalerieDTO;
+use photopro\galeries\core\domain\entities\Galerie;
+use photopro\galeries\core\domain\entities\GaleriePhoto;
+
+interface GalerieRepositoryInterface
+{
+    public function create(GalerieDTO $galerie): Galerie;
+
+    public function addPhotoToGalerie(GaleriePhoto $galeriePhoto): void;
+
+    public function deletePhotoFromGalerie(string $photoId, string $galerieId): void;
+    public function getGalleryPreview(string $galleryId, string $userId): array;
+    public function publishGallery(string $galleryId, string $userId): void;
+
+}
